eca400521764ade0dc4991592ce1531cbe2cfa49
Artur <arturgadyan@mail.ru>
2021-04-08 15:19:59 +0400

    Logging in with Scrapy FormRequest

---
+++ quotetutorial/quotetutorial/backup.py
@@ -0,0 +1,26 @@
+
+import scrapy
+from ..items import QuotetutorialItem
+
+class QuoteSpider(scrapy.Spider):
+    name = "quotes"
+    page_number = 2
+    start_urls=[
+        'http://quotes.toscrape.com/page/1/'
+    ]
+    def parse(self, response):
+        items=QuotetutorialItem()
+        all_div_quotes=response.css("div.quote")
+        for i in all_div_quotes:
+            title=i.css('span.text::text').extract()
+            author=i.css('.author::text').extract()
+            tag =i.css('.tag::text').extract()
+            items['title']=title
+            items['author'] = author
+            items['tag'] = tag
+
+            yield items
+        next_page = 'http://quotes.toscrape.com/page/' + str(QuoteSpider.page_number) + '/'
+        if QuoteSpider.page_number < 11:
+            QuoteSpider.page_number += 1
+            yield response.follow(next_page, callback = self.parse)
--- quotetutorial/quotetutorial/spiders/quotes_spider.py
+++ quotetutorial/quotetutorial/spiders/quotes_spider.py
@@ -1,25 +1,34 @@
 import scrapy
+from scrapy.http import FromRequest
+from scrapy.utils.response import open_in_browser
 from ..items import QuotetutorialItem
 
 class QuoteSpider(scrapy.Spider):
     name = "quotes"
-    page_number = 2
     start_urls=[
-        'http://quotes.toscrape.com/page/1/'
+        'http://quotes.toscrape.com/login'
     ]
     def parse(self, response):
-        items=QuotetutorialItem()
+        token = response.css('from input::attr(value)').extract_first()
+        return FromRequest.from_response(response, formdata={
+            'csrf_tokan' : token,
+            'username' : 'your login',
+            'password' : 'your password'
+        }, callback = self.start_scraping)
+
+    def start_scraping(self, response):
+        open_in_browser(response)
+        items = QuotetutorialItem()
+
         all_div_quotes=response.css("div.quote")
+        
         for i in all_div_quotes:
             title=i.css('span.text::text').extract()
             author=i.css('.author::text').extract()
             tag =i.css('.tag::text').extract()
+
             items['title']=title
             items['author'] = author
             items['tag'] = tag
 
             yield items
-        next_page = 'http://quotes.toscrape.com/page/' + str(QuoteSpider.page_number) + '/'
-        if QuoteSpider.page_number < 11:
-            QuoteSpider.page_number += 1
-            yield response.follow(next_page, callback = self.parse)
